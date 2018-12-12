843e27f8c48cb0138c650a62944915edc31a8e17
Brent Keller <brentkeller@gmail.com>
2018-12-12 00:11:58 -0500

    Animate bubble shrinking

--- dist/index.js
+++ dist/index.js
@@ -8,48 +8,59 @@ var config = {
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
+var graphics;
+var circles;
 var game = new Phaser.Game(config);
+var circleRadius = 45;
 
 function preload() {
-  this.load.audio("theme", ["PopBanner.mp3"]);
+  this.load.audio("pop", ["PopBanner.mp3"]);
 }
 
 function create() {
-  var graphics = this.add.graphics({ fillStyle: { color: 0xcff1fa } });
-  var sfx = this.sound.add("theme");
+  graphics = this.add.graphics({ fillStyle: { color: 0xcff1fa } });
+  var sfx = this.sound.add("pop");
 
-  var circles = [];
+  circles = [];
 
   for (var x = 0; x < 8; x++) {
     circles[x] = [];
     for (var y = 0; y < 6; y++) {
-      circles[x][y] = new Phaser.Geom.Circle(50 + x * 100, 50 + y * 100, 45);
+      circles[x][y] = new Phaser.Geom.Circle(
+        50 + x * 100,
+        50 + y * 100,
+        circleRadius
+      );
     }
   }
 
   this.input.on("pointerdown", function(pointer) {
     var x = Math.floor(pointer.x / 100);
     var y = Math.floor(pointer.y / 100);
+    var targetCircle = circles[x][y];
 
-    circles[x][y].setEmpty();
-    sfx.play();
-
-    redraw();
+    if (targetCircle.radius > 0) {
+      targetCircle.radius--;
+      sfx.play();
+    }
   });
+}
 
-  redraw();
+function update() {
+  graphics.clear();
 
-  function redraw() {
-    graphics.clear();
-
-    for (var x = 0; x < 8; x++) {
-      for (var y = 0; y < 6; y++) {
-        graphics.fillCircleShape(circles[x][y]);
-      }
+  for (var x = 0; x < 8; x++) {
+    for (var y = 0; y < 6; y++) {
+      var circle = circles[x][y];
+      if (circle.radius > 0 && circle.radius < circleRadius)
+        circle.radius = circle.radius * 0.7;
+      if (circle.radius < 2) circle.setEmpty();
+      graphics.fillCircleShape(circle);
     }
   }
 }
